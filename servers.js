4fa8f28eea448bf138dc59c1fe1ebbba88e72a38
Dioniii <90280629+Dioniii@users.noreply.github.com>
2025-04-02 12:05:52 +0200

    Check lidhjen me databaz + Show all users API

--- servers.js
+++ servers.js
@@ -12,3 +12,24 @@ app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`SERVER IS RUNNING ON ${PORT}`));
+
+// SHOW ALL USERS API - Kontrollim i lidhjes me databaz
+
+app.get("/users", async(req,res) =>{
+    try{
+        const pool = await poolPromise;
+        const result = await pool.request().query(
+            "Select * from HotelManagement.dbo.users"
+        );
+        res.status(200).json({
+            success: true,
+            data: result.recordset
+        });
+    }catch(error){
+        res.status(404).json({
+            success:false,
+            error: error.message
+        })
+    }
+})
+
