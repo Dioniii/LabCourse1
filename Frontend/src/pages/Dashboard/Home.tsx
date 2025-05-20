5868bf51ea19ce9db5f044f46febacaaf5beab4e
AnuarShevelli <anuarshevelli@gmail.com>
2025-05-20 14:34:48 +0200

    Dizajnimi i faqes Dashboard

--- Frontend/src/pages/Dashboard/Home.tsx
+++ Frontend/src/pages/Dashboard/Home.tsx
@@ -1,10 +1,10 @@
-import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
-import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
-import StatisticsChart from "../../components/ecommerce/StatisticsChart";
-import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
-import RecentOrders from "../../components/ecommerce/RecentOrders";
-import DemographicCard from "../../components/ecommerce/DemographicCard";
+import RecentOrdersForRooms from "../../layout/RecentOrdersForRooms";
+import RevenueStats from "../../layout/RevenueStats";
+import CheckinStatus from "../../layout/CheckinStatus";
+import BasicTableOne from "../../components/tables/BasicTables/BasicTableOne";
+import Calendar from "../../pages/Calendar";
 import PageMeta from "../../components/common/PageMeta";
+import RecentOrdersForBookings from "../../layout/RecentOrdersForBookings";
 
 export default function Home() {
   return (
@@ -14,26 +14,36 @@ export default function Home() {
         description="This is a hotel booking system"
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="col-span-12 space-y-6 xl:col-span-7">
-          <EcommerceMetrics />
-
-          <MonthlySalesChart />
+        {/* Tabela me scroll dhe gjerësi të plotë */}
+        <div className="col-span-12 xl:col-span-12 space-y-6">
+        <div className="w-full max-h-[400px] overflow-y-auto">
+          <BasicTableOne />
         </div>
+      </div>
 
+      {/* Kalendar i zgjeruar plotësisht me scroll */}
+      <div className="col-span-12 xl:col-span-12">
+        <div className="w-full h-[400px] overflow-y-auto">
+          <Calendar />
+        </div>
+      </div>
+
+
+        {/* Komponentët tjerë */}
         <div className="col-span-12 xl:col-span-5">
-          <MonthlyTarget />
+          <CheckinStatus />
         </div>
 
         <div className="col-span-12">
-          <StatisticsChart />
+          <RecentOrdersForRooms />
         </div>
 
         <div className="col-span-12 xl:col-span-5">
-          <DemographicCard />
+          <RevenueStats />
         </div>
 
         <div className="col-span-12 xl:col-span-7">
-          <RecentOrders />
+          <RecentOrdersForBookings />
         </div>
       </div>
     </>
